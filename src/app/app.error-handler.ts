107e5b2cca3fe4561783d91ccd99b8cec4c069f2
paulovieiradasilva <paulovieiradasilva@hotmail.com.br>
2019-02-17 00:11:09 -0300

    tratamento de erros

---
+++ src/app/app.error-handler.ts
@@ -0,0 +1,15 @@
+import { Observable } from 'rxjs/Observable';
+import { Response } from '@angular/http';
+
+export class ErrorHandler {
+    static hasError(error: Response | any) {
+        let errorMessage: string;
+        if (error instanceof Response) {
+            errorMessage = `Erro ${error.status} ao obter a URL ${error.url} - ${error.statusText}`;
+        } else {
+            errorMessage = error.toString();
+        }
+        console.log(errorMessage);
+        return Observable.throw(errorMessage);
+    }
+}
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,9 +1,11 @@
+import { ErrorHandler } from './../app.error-handler';
 import { MEAT_API } from './../../app.api';
 import { Injectable } from '@angular/core';
 import { Restaurant } from './restaurant/restaurant.model';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class RestaurantsService {
@@ -11,7 +13,7 @@ export class RestaurantsService {
   constructor(private http: Http) { }
 
   restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json());
+    return this.http.get(`${MEAT_API}/restaurants`).map((response) => response.json()).catch(ErrorHandler.hasError);
   }
 
 }
