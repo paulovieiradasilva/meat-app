c2ecfd92130a6616e3166be28e52fe3303e5de54
paulovieiradasilva <paulovieiradasilva@hotmail.com.br>
2019-02-16 23:56:03 -0300

    utilizando o modulo http no servico de restaurants

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,33 +1,17 @@
+import { MEAT_API } from './../../app.api';
 import { Injectable } from '@angular/core';
 import { Restaurant } from './restaurant/restaurant.model';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class RestaurantsService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
-  // tslint:disable-next-line:member-ordering
-  data: Restaurant[] = [
-    {
-      id: 'bread-bakery',
-      name: 'Bread & Bakery',
-      category: 'Bakery',
-      deliveryEstimate: '25m',
-      rating: 4.9,
-      imagePath: 'assets/img/restaurants/breadbakery.png',
-    },
-    {
-      id: 'burger-house',
-      name: 'Burger House',
-      category: 'Hamburgers',
-      deliveryEstimate: '100m',
-      rating: 3.5,
-      imagePath: 'assets/img/restaurants/burgerhouse.png',
-    }
-  ];
-
-  restaurants(): Restaurant[] {
-    return this.data;
+  restaurants(): Observable<Restaurant[]> {
+    return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json());
   }
 
 }
