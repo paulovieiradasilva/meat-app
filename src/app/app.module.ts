db7115c9ccec2ebc0cde74ff25a292ba80737c36
paulovieiradasilva <paulovieiradasilva@hotmail.com.br>
2019-02-20 23:35:12 -0300

    Realizando o Pré-Carregamento de Todos os Módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
 
 import { ROUTES } from './app.routes';
@@ -38,7 +38,7 @@ import { OrderSummaryComponent } from './order/order-summary/order-summary.compo
     HttpModule,
     RouterModule,
     SharedModule.forRoot(),
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
