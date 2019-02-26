09f05956a7bfde1ec228453e7520bdc86cd97f9e
paulovieiradasilva <paulovieiradasilva@hotmail.com.br>
2019-02-25 23:47:28 -0300

    Utilizando Zonas

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core';
 import { NotificationService } from './shared/messages/notification.service';
 import { LoginService } from './security/login/login.service';
 
@@ -7,7 +7,7 @@ import { LoginService } from './security/login/login.service';
 @Injectable()
 export class ApplicationErrorHandler extends ErrorHandler {
 
-    constructor(private ns: NotificationService, private injector: Injector) {
+    constructor(private ns: NotificationService, private injector: Injector, private zone: NgZone) {
         super()
     }
 
@@ -15,22 +15,24 @@ export class ApplicationErrorHandler extends ErrorHandler {
 
         if (errorResponse instanceof HttpErrorResponse) {
             const message = errorResponse.error.message
-            switch (errorResponse.status) {
-                case 401:
-                    this.injector.get(LoginService).toIdentify()
-                    break;
+            this.zone.run(() => {
+                switch (errorResponse.status) {
+                    case 401:
+                        this.injector.get(LoginService).toIdentify()
+                        break;
 
-                case 403:
-                    this.ns.notify(message || "Não autorizado.")
-                    break;
+                    case 403:
+                        this.ns.notify(message || "Não autorizado.")
+                        break;
 
-                case 404:
-                    this.ns.notify(message || "Recurso não encontrado. Verifique o console para mais detalhes")
-                    break;
+                    case 404:
+                        this.ns.notify(message || "Recurso não encontrado. Verifique o console para mais detalhes")
+                        break;
 
-                default:
-                    break;
-            }
+                    default:
+                        break;
+                }
+            })
         }
         super.handleError(errorResponse)
     }
